df923303bbecac2ed82b3e90104e2dbffa1cd895
danko beribak <beribakdanko@gmail.com>
2022-11-08 10:22:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("Hello world!!!")
+
+// SELECTING ELEMENTS
+const header = document.querySelector("#header");
+const list = document.querySelector(".world");
+const list_items = document.querySelectorAll(".country");
+const parent = document.querySelector(".parent h3.grand-child");
+
+// DOM MANIPULATION
+// list.style.display = "none";
+// list.classList.add("hello");
+// console.log(list.dataset.danko) 
+// const header = document.getElementById("header");
+// header.innerText = "Hello Danko!!!";
+// list.insertAdjacentHTML("beforeend", '<li class="country">Macedonia</li>')
+
+// EVENTS LISTENERS
+// list.addEventListener("click", () => {
+//     list.classList.toggle("danko");
+// })
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("Hello youuu!")
+// })
+
+
